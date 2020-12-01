225c16f8935bd1f74bd9bbefd2c7425286a10eb7
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-12-01 15:49:07 +0300

    nuxtptoject

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>About page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga commodi architecto doloribus, autem praesentium nobis dolore quos vitae delectus, culpa dicta excepturi nostrum vero iste totam obcaecati aliquid aspernatur?</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsum sint eius reprehenderit ea placeat eos, facilis natus libero commodi dolorum vero laboriosam accusantium ducimus repudiandae delectus cupiditate vitae blanditiis.</p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus voluptates hic quae pariatur exercitationem, perferendis nobis vero id rem et veritatis voluptatibus! Quia nulla soluta aliquid, officiis cum fugiat unde.</p>
+
+
+    </section>
+</template>
+
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
---
+++ pages/users/_id.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <h1>{{user.name}}</h1>
+
+        <hr>
+
+        <h3>{{user.email}}</h3>
+    </section>
+</template>
+
+<script>
+export default {
+    validate({params}) {
+        console.log(params);
+        return /^\d+$/.test(params.id)
+    },
+    async asyncData({$axios, params}) {
+        const user = await $axios.$get('http://jsonplaceholder.typicode.com/users/' + params.id)
+        return {user}
+    }
+}
+</script>
